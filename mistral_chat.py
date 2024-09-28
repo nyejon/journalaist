b0c10b56ca21472bb329ec3afa167470bf51c385
Ferdinand Schenck <fwschenck@gmail.com>
2024-09-28 19:36:44 +0200

    Add text interleave

--- mistral_chat.py
+++ mistral_chat.py
@@ -7,6 +7,12 @@ from PIL import Image
 import mistral_files
 import prompts
 
+if "uploader_visible" not in st.session_state:
+    st.session_state["uploader_visible"] = False
+def show_upload(state:bool):
+    st.session_state["uploader_visible"] = state
+
+
 if 'page' not in st.session_state:
     st.session_state.page = 'chat'
 
@@ -70,6 +76,13 @@ if st.session_state.page == 'chat':
     if "uploaded_files" not in st.session_state:
         st.session_state.uploaded_files = []
 
+    
+    if "current_file" not in st.session_state:
+        st.session_state.current_file = []
+
+    if "n_pictures" not in st.session_state:
+        st.session_state.n_pictures = 0
+
     # Add system prompt as a UserMessage if it doesn't exist
     if st.session_state["system_prompt"] and not any(
         message.role == "system" for message in st.session_state.messages
@@ -81,18 +94,6 @@ if st.session_state.page == 'chat':
         intro_message = AssistantMessage(content="Hi! What did you get up to today?")
         st.session_state.messages.append(intro_message)
 
-    st.session_state.n_pictures = 0
-    uploaded_files = st.file_uploader("Choose images...", type=["jpg", "png"], accept_multiple_files=True)
-    if uploaded_files:
-        if not os.path.exists("./stories"):
-            os.makedirs("./stories")
-
-        cols = st.columns(len(uploaded_files))
-        for col, uploaded_file in zip(cols, uploaded_files):
-            st.session_state.n_pictures += 1
-            image = Image.open(uploaded_file)
-            col.image(image, use_column_width=True)
-            image.convert("RGB").save(f"./stories/picture_{st.session_state.n_pictures}.jpg")
 
     for message in st.session_state.picture_messages:
         if message.role != "system":  # Skip system messages for UI
@@ -104,27 +105,6 @@ if st.session_state.page == 'chat':
             with st.chat_message(message.role):  # Use dot notation here
                 st.markdown(message.content)  # And here
 
-
-    if st.session_state.uploaded_files != uploaded_files and uploaded_files:
-
-        # print("in here")
-        file_info = mistral_files.handle_files(uploaded_files, client, model=st.session_state["pixtral_model"])
-
-        picture_response = ""
-        message_placeholder = st.empty()
-        if file_info:
-            for response in file_info:
-                picture_response += response.data.choices[0].delta.content or ""
-                message_placeholder.markdown(picture_response + "▌")
-            message_placeholder.markdown(picture_response)
-        else:
-            # Handle the case where response_generator is None
-            st.error("Failed to generate response")
-            message_placeholder.markdown(picture_response)
-
-        st.session_state.picture_messages.append(AssistantMessage(content=picture_response))
-
-        st.session_state.uploaded_files = uploaded_files
 
     if prompt := st.chat_input("What event would you like me to write a story about?"):
         new_message = UserMessage(role="user", content=prompt)
@@ -152,13 +132,45 @@ if st.session_state.page == 'chat':
             message_placeholder.markdown(full_response)
         st.session_state.messages.append(AssistantMessage(content=full_response))
 
+    col1, col2 = st.columns([1,2])
 
-    # Add end conversation button outside of the chat input condition
-    end_conversation = st.button("End Conversation")
+    with col1:
+        end_conversation = st.button("End Conversation")
+    with col2:
+        upload_image = st.button("Upload Image", on_click=show_upload, args=[True])
+
+    
+    #uploaded_files = []
+    if upload_image:
+        if st.session_state["uploader_visible"]:
+            
+            st.session_state.current_file = st.file_uploader("Choose images...", type=["jpg", "png"], accept_multiple_files=False)
+            #st.session_state.messages.append(SystemMessage(content=f"Uploaded {len(uploaded_files)} images"))
+            print("Here")
+
+    print("Current file",  st.session_state.current_file)
+    if st.session_state.current_file:
+        if not os.path.exists("./stories"):
+            os.makedirs("./stories")
+
+        cols = st.columns(len(st.session_state.current_file))
+        for col, uploaded_file in zip(cols, st.session_state.current_file):
+            st.session_state.n_pictures += 1
+            image = Image.open(uploaded_file)
+            col.image(image, use_column_width=True)
+            image.convert("RGB").save(f"./stories/picture_{st.session_state.n_pictures}.jpg")
+        # Hide the uploader again. 
+        show_upload(False)
+        print("Delete current file")
+        st.session_state.current_file = []
+        
+
 
     if end_conversation:
         st.session_state.page = 'story'
         st.rerun()
+
+
 
 elif st.session_state.page == 'story':
     if 'CONFIG' not in st.session_state:
