d80440abeaa9fc9cb66fbacc4cf2f6dfd8782b22
Ferdinand Schenck <fwschenck@gmail.com>
2024-09-28 20:23:52 +0200

    Add inline images in story

--- story_generation.py
+++ story_generation.py
@@ -6,6 +6,42 @@ import streamlit as st
 import os
 
 import prompts
+import re
+import base64
+from pathlib import Path
+
+def markdown_images(markdown):
+    # example image markdown:
+    # ![Test image](images/test.png "Alternate text")
+    images = re.findall(r'(!\[(?P<image_title>[^\]]+)\]\((?P<image_path>[^\)"\s]+)\s*([^\)]*)\))', markdown)
+    return images
+
+
+def img_to_bytes(img_path):
+    img_bytes = Path(img_path).read_bytes()
+    encoded = base64.b64encode(img_bytes).decode()
+    return encoded
+
+
+def img_to_html(img_path, img_alt):
+    img_format = img_path.split(".")[-1]
+    img_html = f'<img src="data:stories/{img_format.lower()};base64,{img_to_bytes(img_path)}" alt="{img_alt}" style="max-width: 100%;">'
+
+    return img_html
+
+
+def markdown_insert_images(markdown):
+    images = markdown_images(markdown)
+
+    for image in images:
+        image_markdown = image[0]
+        image_alt = image[1]
+        image_path = image[2]
+        if os.path.exists(image_path):
+            markdown = markdown.replace(image_markdown, img_to_html(image_path, image_alt))
+    return markdown
+
+
 
 def story_generation(client):
     # Create columns for horizontal layout
@@ -67,9 +103,18 @@ def story_generation(client):
         )
         story = story_response.choices[0].message.content
 
-        # Format the story as markdown
-        st.markdown(story)
-
-            # Save the story to a markdown file
+        # Save the story to a markdown file
         with open('stories/story.md', 'w') as f:
             f.write(story)
+
+        with open('stories/story.md', "r") as story_file:
+            story = story_file.read()
+
+        story = markdown_insert_images(story)
+        with st.container():
+            st.markdown(story, unsafe_allow_html=True)
+
+        # Format the story as markdown
+        #st.markdown(story)
+
+ 
